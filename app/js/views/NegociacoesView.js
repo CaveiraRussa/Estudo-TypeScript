ef9fa0c982cbd2c0db94f8c7f03720a3c83ed3f4
Luiz Augusto <luiz.m.santiago@hotmail.com>
2020-06-19 11:47:42 -0300

    Primeira parte do curso concluida, inicio da 2º

---
+++ app/js/views/NegociacoesView.js
@@ -0,0 +1,43 @@
+System.register(["./View"], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var View_1, NegociacoesView;
+    return {
+        setters: [
+            function (View_1_1) {
+                View_1 = View_1_1;
+            }
+        ],
+        execute: function () {
+            NegociacoesView = class NegociacoesView extends View_1.View {
+                template(model) {
+                    return `
+        <table class="table table-hover table-bordered">
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                    <th>VOLUME</th>
+                </tr>
+            </thead>
+
+            <tbody>
+            </tbody>
+                ${model.paraArray().map(Negociacao => `
+                    <tr>
+                        <td>${Negociacao.data.getDate()}/${Negociacao.data.getMonth() + 1}/${Negociacao.data.getFullYear()}</td>
+                        <td>${Negociacao.quantidade}</td>
+                        <td>${Negociacao.valor}</td>
+                        <td>${Negociacao.volume}</td>                   
+                    <tr>
+                    `).join('')}
+            <tfoot>
+            </tfoot>
+        </table>               
+        `;
+                }
+            };
+            exports_1("NegociacoesView", NegociacoesView);
+        }
+    };
+});
