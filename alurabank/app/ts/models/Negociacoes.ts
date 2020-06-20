4ff951c67a323cfe6c2bd5ac3f6e0e9b947dea5c
Luiz Augusto <luiz.m.santiago@hotmail.com>
2020-06-20 15:33:06 -0300

    Adicionado decorators e API externa

---
+++ alurabank/app/ts/models/Negociacoes.ts
@@ -0,0 +1,16 @@
+import { Negociacao } from './Negociacao';
+import { logarTempoDeExecucao } from '../helpers/decorators/index';
+
+export class Negociacoes{
+
+    private _negociacoes: Negociacao [] =[];
+
+    adiciona(negociacao: Negociacao){
+        this._negociacoes.push(negociacao);
+    }
+
+    @logarTempoDeExecucao()
+    paraArray(): Negociacao[]{
+        return ([] as Negociacao[]).concat(this._negociacoes);
+    }
+}
